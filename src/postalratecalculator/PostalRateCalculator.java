f8dc676377507a168b4ace201f83f141f7278356
JastajVirdee <jastaj.virdee@mail.mcgill.ca>
2018-02-18 14:25:24 -0500

    added main class and test class

---
+++ src/postalratecalculator/PostalRateCalculator.java
@@ -0,0 +1,7 @@
+package postalratecalculator;
+
+public class PostalRateCalculator {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ test/postalratecalculator/PostalRateTest.java
@@ -0,0 +1,8 @@
+package postalratecalculator;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PostalRateTest {
+	
+}
