684ba32ec50f27696e6f24019c927ebff7306208
JastajVirdee <jastaj.virdee@mail.mcgill.ca>
2018-02-18 19:32:47 -0500

    added a few intial things

--- src/postalratecalculator/PostalRateCalculator.java
+++ src/postalratecalculator/PostalRateCalculator.java
@@ -1,7 +1,9 @@
 package postalratecalculator;
 
 public class PostalRateCalculator {
+	
 	public static void main(String[] args) {
 		
 	}
+	
 }
--- test/postalratecalculator/PostalRateTest.java
+++ test/postalratecalculator/PostalRateTest.java
@@ -2,7 +2,22 @@ package postalratecalculator;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
+import postalratecalculator.PostalRateCalculator;
 
 public class PostalRateTest {
+	
+	private PostalRateCalculator calculator = new PostalRateCalculator();
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
 	
 }
